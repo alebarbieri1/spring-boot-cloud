7262e9487db20b228cff68478cc2775f7a895ce0
Alexandre Barbieri <Alexandre.Barbieri@dieboldnixdorf.com>
2019-07-03 14:08:30 -0300

    Spring Cloud Bus + Fault Tolerance with Hystrix

--- limits-service/src/main/java/com/spring/course/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/spring/course/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,16 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.spring.course.microservices.limitsservice.bean.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
-	
+
 	@Autowired
 	private Configuration configuration;
 
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("");
+	}
+
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
 	}
 }
